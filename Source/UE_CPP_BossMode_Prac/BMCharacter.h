299a397152e3680de9b16bfb23252cc35d42b422
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-16 04:48:05 +0200

    declared members and methods

---
+++ Source/UE_CPP_BossMode_Prac/BMCharacter.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "BMCharacter.generated.h"
+
+UCLASS()
+class UE_CPP_BOSSMODE_PRAC_API ABMCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	USkeletalMeshComponent* FPMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	USkeletalMeshComponent* FPGunMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess="true"))
+	UCameraComponent* FPCameraComponent;
+
+public:
+	// Sets default values for this character's properties 
+	ABMCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Val);
+
+	void MoveRight(float Val);
+
+	void TurnAtRate(float Rate);
+
+	void LookUpAtRate(float Rate);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseLookUpRate;	
+};

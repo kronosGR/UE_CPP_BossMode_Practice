26a0af6d5103482c76305e9c057c34753fee6390
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-19 06:07:42 +0200

    implementation

--- Source/UE_CPP_BossMode_Prac/BMBossCharacter.cpp
+++ Source/UE_CPP_BossMode_Prac/BMBossCharacter.cpp
@@ -3,32 +3,31 @@
 
 #include "BMBossCharacter.h"
 
+#include "GameFramework/PawnMovementComponent.h"
+
 // Sets default values
 ABMBossCharacter::ABMBossCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Health = 100.f;
+
+	GetMovementComponent()->NavAgentProps.AgentHeight = 320.f;
+	GetMovementComponent()->NavAgentProps.AgentRadius = 160.f;
 }
 
-// Called when the game starts or when spawned
-void ABMBossCharacter::BeginPlay()
+float ABMBossCharacter::TakeDamage(float Damage, const FDamageEvent& DamagEvent, AController* EventInstigator,
+                                   AActor* DamageCauser)
 {
-	Super::BeginPlay();
-	
+	Super::TakeDamage(Damage, DamagEvent, EventInstigator, DamageCauser);
+
+	Health -= Damage;
+
+	return Damage;
 }
 
-// Called every frame
-void ABMBossCharacter::Tick(float DeltaTime)
+const float ABMBossCharacter::GetHealth()
 {
-	Super::Tick(DeltaTime);
-
+	return Health;
 }
-
-// Called to bind functionality to input
-void ABMBossCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-

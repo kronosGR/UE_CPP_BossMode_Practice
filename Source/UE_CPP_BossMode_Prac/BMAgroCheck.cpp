d208f2f454c441e0f45465e047cab0942d8aebb8
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-21 11:57:10 +0200

    fix a bug on compilation

--- Source/UE_CPP_BossMode_Prac/BMAgroCheck.cpp
+++ Source/UE_CPP_BossMode_Prac/BMAgroCheck.cpp
@@ -3,3 +3,20 @@
 
 #include "BMAgroCheck.h"
 
+void UBMAgroCheck::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	if (ThisTree == nullptr || ThisController == nullptr || ThisAicCharacter == nullptr)
+	{
+		ThisTree = OwnerComp.GetCurrentTree();
+		ThisController = Cast<ABMAIController>(OwnerComp.GetAIOwner());
+		ThisAicCharacter = Cast<ABMBossCharacter>(ThisController->GetPawn());
+
+		if (ThisTree == nullptr || ThisController == nullptr || ThisAicCharacter == nullptr)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Warning Argo Service performed on invalid AI"));
+			return;
+		}
+	}
+}
--- Source/UE_CPP_BossMode_Prac/UE_CPP_BossMode_Prac.Build.cs
+++ Source/UE_CPP_BossMode_Prac/UE_CPP_BossMode_Prac.Build.cs
@@ -8,7 +8,7 @@ public class UE_CPP_BossMode_Prac : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 

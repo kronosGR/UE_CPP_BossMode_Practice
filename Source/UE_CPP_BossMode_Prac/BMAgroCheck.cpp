defe9da8d4aaa37aca2d0e4f301d4203d1a579ec
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-22 06:03:00 +0200

    implemented Tick method

--- Source/UE_CPP_BossMode_Prac/BMAgroCheck.cpp
+++ Source/UE_CPP_BossMode_Prac/BMAgroCheck.cpp
@@ -3,6 +3,8 @@
 
 #include "BMAgroCheck.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
+
 void UBMAgroCheck::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
@@ -18,5 +20,28 @@ void UBMAgroCheck::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory
 			UE_LOG(LogTemp, Warning, TEXT("Warning Argo Service performed on invalid AI"));
 			return;
 		}
+	}
+
+	FCollisionQueryParams SphereSweepParams(FName(TEXT("AgroCheckSweep")), true, ThisAicCharacter);
+	FCollisionObjectQueryParams ObjectQuery(ECC_GameTraceChannel1);
+
+	FHitResult HitOut(ForceInit);
+
+	DrawDebugSphere(ThisAicCharacter->GetWorld(), ThisAicCharacter->GetActorLocation(), 1500, 12, FColor::Red, false,
+	                4.f);
+
+	bool bResult = ThisAicCharacter->GetWorld()->SweepSingleByObjectType(HitOut, ThisAicCharacter->GetActorLocation(),
+	                                                                     ThisAicCharacter->GetActorLocation() + FVector(
+		                                                                     0.f, 0.f, 10.f), FQuat(), ObjectQuery,
+	                                                                     FCollisionShape::MakeSphere(1500),
+	                                                                     SphereSweepParams);
+	if (bResult)
+	{
+		ThisController->GetBlackboard()->SetValueAsObject(TEXT("TargetToFollow"), HitOut.GetActor());
+		ThisController->GetBlackboard()->SetValueAsVector(TEXT("HomeLocation"), ThisAicCharacter->GetActorLocation());
+		ThisController->GetBlackboard()->SetValueAsVector(TEXT("TargetLocation"), HitOut.GetActor()->GetActorLocation());
+	} else
+	{
+		ThisController->GetBlackboard()->SetValueAsObject(TEXT("TargetToFollow"),nullptr);
 	}
 }
